65b436b0da7618b5b0de5c2b24a4b32162b22607
LAPTOP-U9LNG27H\essences24775257 <chiaochiao1106@gmail.com>
2022-11-02 19:09:25 +0800

    Customizing theme# chakara 引入

---
+++ src/chakra/theme.ts
@@ -0,0 +1,11 @@
+// Import the extendTheme function
+import { extendTheme } from "@chakra-ui/react";
+
+// Extend the theme to include custom colors, fonts, etc
+export const theme = extendTheme({
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+});
