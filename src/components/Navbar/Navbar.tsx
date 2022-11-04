b0d36f807d68f18532a027fdab206204a48b8d2e
LAPTOP-U9LNG27H\essences24775257 <chiaochiao1106@gmail.com>
2022-11-04 17:24:24 +0800

    import image comp into nav comp for logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,25 @@
 import * as React from "react";
+import { Flex, Image } from "@chakra-ui/react";
 
 interface IAppProps {}
 
 const Navbar: React.FunctionComponent = (props) => {
-  return <div>nav</div>;
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex>
+        <Image
+          height="30px"
+          src="/images/redditFace.svg"
+          alt="redditFace.svg"
+        />
+        <Image
+          height="30px"
+          src="/images/redditText.svg"
+          alt="redditText.svg"
+        />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Navbar;
